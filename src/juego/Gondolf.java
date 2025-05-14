df07c67684ab0b224d09642c664b5b53aa6b5c1b
Ariel Espinoza <arielespinozaaldave@gmail.com>
2025-05-14 20:53:11 -0300

    Se creo clase gondolf

---
+++ src/juego/Gondolf.java
@@ -0,0 +1,22 @@
+package juego;
+
+public class Gondolf {
+	double x;
+	double y;
+	double velocidad;
+	int magia;
+	int vida;
+	public Gondolf(double x, double y,double velocidad, int magia, int vida) {
+		 this.x = x;
+		 this.y = y;
+		 this.velocidad = velocidad;
+		 this.magia = magia;
+		 this.vida = vida;
+	 }
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
