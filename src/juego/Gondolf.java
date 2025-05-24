79168d135e718a42d33ca62115f36c1474de9fbe
Ariel Espinoza <arielespinozaaldave@gmail.com>
2025-05-24 17:35:06 -0300

    Se modifico el metodo mover para que el mago no se salga de la pantalla ni pise el menu

--- src/juego/Gondolf.java
+++ src/juego/Gondolf.java
@@ -14,30 +14,39 @@ public class Gondolf {
 	public Gondolf(double xInicial, double yInicial) {
 		 this.x = xInicial;
 		 this.y = yInicial;
-		 this.velocidad = 3.0;
+		 this.velocidad = 5.0;
 		 this.magia = 100;
 		 this.vida = 100;
-		 this.ancho = 20;
-		 this.alto = 20;
+		 this.ancho = 16;
+		 this.alto = 30;
 	 }
 	// Metodo para mover al mago al presionar teclas "WASD" o flechas
 	public void mover(Entorno entorno) {
-		if (entorno.estaPresionada('W') || entorno.estaPresionada(entorno.TECLA_ARRIBA)) {
-			y -= velocidad;
-		}
-		if (entorno.estaPresionada('S') || entorno.estaPresionada(entorno.TECLA_ABAJO)) {
-			y += velocidad;
-		}
-		if (entorno.estaPresionada('A') || entorno.estaPresionada(entorno.TECLA_IZQUIERDA)) {
-			x -= velocidad;
-		}
-		if (entorno.estaPresionada('D') || entorno.estaPresionada(entorno.TECLA_DERECHA)) {
-			x += velocidad;
-		}
+		double nuevoX = x;
+	    double nuevoY = y;
+	    if (entorno.estaPresionada('W') || entorno.estaPresionada(entorno.TECLA_ARRIBA)) {
+	        nuevoY -= velocidad;
+	    }
+	    if (entorno.estaPresionada('S') || entorno.estaPresionada(entorno.TECLA_ABAJO)) {
+	        nuevoY += velocidad;
+	    }
+	    if (entorno.estaPresionada('A') || entorno.estaPresionada(entorno.TECLA_IZQUIERDA)) {
+	        nuevoX -= velocidad;
+	    }
+	    if (entorno.estaPresionada('D') || entorno.estaPresionada(entorno.TECLA_DERECHA)) {
+	        nuevoX += velocidad;
+	    }
+		// Limites: no se sale de la pantalla ni entra al menú
+	    if (nuevoX - ancho / 2 >= 0 && nuevoX + ancho / 2 <= 600) {
+	        x = nuevoX;
+	    }
+	    if (nuevoY - alto / 2 >= 0 && nuevoY + alto / 2 <= 600) {
+	        y = nuevoY;
+	    }
 	}
 	// Metodo para dibujar al mago, de manera provisoria se dibuja un cirulo rojo
 	public void dibujar(Entorno entorno) {
-		entorno.dibujarCirculo(x, y, alto, Color.red);
+		entorno.dibujarRectangulo(x, y, ancho,alto,0, Color.red);
 	}
 	// Metodo para recibir daño
 	public void recibirDanio(int cantidad) {
@@ -61,6 +70,5 @@ public class Gondolf {
     public double getY() { return y; }
     public int getVida() { return vida; }
     public int getMagia() { return magia; }
-
 
 }
