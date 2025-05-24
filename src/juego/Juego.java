5468244a25e4306033ca80f6a63a6a748a51937a
Ariel Espinoza <arielespinozaaldave@gmail.com>
2025-05-24 17:27:36 -0300

    Se implementan metodos y variables en la clase juego para pruebas

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -12,11 +12,56 @@ public class Juego extends InterfaceJuego
 	
 	// Variables y métodos propios de cada grupo
 	// ...
-	Gondolf mago = new Gondolf(400,300); // Se crea el objeto mago con posicion definida en el centro
+	
+	// Metodo para generar posiciones fuera de la pantalla para generar murcielagos
+	private double[] generarPosicionAleatoriaFueraDePantalla() {
+	    int borde = (int)(Math.random() * 4); // 0: arriba, 1: abajo, 2: izquierda, 3: derecha
+	    double x = 0;
+	    double y = 0;
+
+	    if (borde == 0) { // arriba
+	        x = Math.random() * 600;
+	        y = -20;
+	    } else if (borde == 1) { // abajo
+	        x = Math.random() * 600;
+	        y = altoPantalla + 20;
+	    } else if (borde == 2) { // izquierda
+	        x = -20;
+	        y = Math.random() * altoPantalla;
+	    } else if (borde == 3) { // derecha
+	        x = 620; // justo antes del menú
+	        y = Math.random() * altoPantalla;
+	    }
+	    return new double[]{x, y};
+	}
+
+	// Variables de murcielagos
+	private int totalGenerados = 0;
+	private int maxGenerados = 50;
+	double velocidadMurcielagos = 2.0;
+	private Murcielago[] enemigos = new Murcielago[10];
+	private int tiempoUltimoMurcielago = 0;
+	private int intervalo = 1000;
+	
+	
+	//Variables de juego
+	private boolean juegoTerminado = false;
+	int anchoPantalla = 800;
+	int altoPantalla = 600;
+	
+	
+	// Variable de mago
+	Gondolf mago = new Gondolf(anchoPantalla/2,altoPantalla/2); // Se crea el objeto mago con posicion definida en el centro
+	
+	// Variables de menu
+	Menu menu = new Menu(600, 200, altoPantalla);
+	private int enemigosEliminados = 0;
+	
+	
 	Juego()
 	{
 		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this, "El camino de Gondolf - Grupo XX - v1 <-- PONER EL NUMERO DEL GRUPO!!!", 800, 600);
+		this.entorno = new Entorno(this, "El camino de Gondolf - Grupo 7 - v1", anchoPantalla, altoPantalla);
 		
 		// Inicializar lo que haga falta para el juego
 		// ...
@@ -35,8 +80,62 @@ public class Juego extends InterfaceJuego
 	{
 		// Procesamiento de un instante de tiempo
 		// ...
-		mago.dibujar(entorno); // Se dibuja al mago en la pantalla
-		mago.mover(entorno); // Metodo para mover al mago
+		
+		// Verifica que el mago este vivo
+		if (mago.getVida()<=0) {
+		    juegoTerminado = true;
+		}
+		// Verifica el tiempo en milisegundos por cada tick
+		int tiempoActual = entorno.tiempo();
+		
+		// Si el mago esta vivo continua el juego
+		if (!juegoTerminado) {
+			menu.actualizarSeleccion(entorno);
+			menu.dibujar(entorno, mago.getVida(), mago.getMagia(), enemigosEliminados);
+			mago.dibujar(entorno); // Se dibuja al mago en la pantalla
+			mago.mover(entorno); // Metodo para mover al mago
+		
+			// Generador de murcielagos cuando en pantalla 10 o menos murcielagos y menos de 50 en total
+			if (tiempoActual - tiempoUltimoMurcielago >= intervalo && totalGenerados < maxGenerados) {
+			    for (int i = 0; i < enemigos.length; i++) {
+			        if (enemigos[i] == null) {
+			            double[] pos = generarPosicionAleatoriaFueraDePantalla();
+			            enemigos[i] = new Murcielago(pos[0], pos[1], velocidadMurcielagos);
+			            tiempoUltimoMurcielago = tiempoActual;
+			            totalGenerados++;
+			            break;
+			        }
+			    }
+			}
+		
+			for (int i = 0; i < enemigos.length; i++) {
+		        Murcielago m = enemigos[i];
+		        
+		        // Se verifica que cada murcielago siga vivo
+		        if (m != null && m.getVivo()) {
+		            m.moverHacia(mago);
+		            
+		            // Los murcielagos se dibujan si no estan en la zona del menu
+		            if (m.getX() < 600) {
+		                m.dibujar(entorno); 
+		            }
+	
+		            // Colisión con Gondolf
+		            if (m.colisionaCon(mago.getX(), mago.getY(), 20)) {
+		                mago.recibirDanio(10);
+		                enemigos[i] = null; // lo eliminamos
+		                enemigosEliminados++;
+		                continue; // no chequeamos hechizos si ya murió
+		            }
+		        }
+		    }
+		}
+		// Si el mago muerte muestra el siguiente mensaje en pantalla
+		else {
+	        entorno.cambiarFont("Arial", 36, java.awt.Color.RED);
+	        entorno.escribirTexto("¡Has perdido!", 300, 300);
+	    }
+
 	} 
 	
 
